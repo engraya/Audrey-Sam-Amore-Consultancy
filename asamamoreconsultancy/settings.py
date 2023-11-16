a3637e41b29452254471eaab19f18fbd94478531
Engraya <ahmadyakubuaya@gmail.com>
2023-11-16 20:15:47 +0100

    Extensions Del

--- asamamoreconsultancy/settings.py
+++ asamamoreconsultancy/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'django_extensions',
+    # 'django_extensions',
 
     'dbbackup',  # django-dbbackup
 
