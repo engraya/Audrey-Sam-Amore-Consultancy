1be90b8f6efa2b754061661606f52efc493ef15c
Engraya <ahmadyakubuaya@gmail.com>
2024-01-06 12:16:13 +0100

    Reconfig Database Dependencies

--- asamamoreconsultancy/settings.py
+++ asamamoreconsultancy/settings.py
@@ -131,6 +131,7 @@ STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
+
 # Media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
